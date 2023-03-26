3c6f4a3e0c97d15f8d017a40014a9f73c1fa7170
Collin Chan <omiyage4949@gmail.com>
2023-03-25 17:04:38 -0700

    Begin work on reorder button

--- src/App.js
+++ src/App.js
@@ -3,23 +3,34 @@ import './App.css';
 import { Gallery } from './Gallery.js';
 
 function App() {
+  const url = 'http://jsonplaceholder.typicode.com/photos';
+  const [ photoData, setData ] = useState(null);
+  const [ loading, setLoading ] = useState(true);
+  const [ error, setError ] = useState(false);
+
+  useEffect(() => {
+    setLoading(true);
+    setError(false);
+    fetch(url)
+    .then((response) => {
+          return response.json();
+    })
+    .then((photoData) => {
+          console.log(photoData[0]);
+          photoData = photoData.slice(1, 100);
+          setData(photoData);
+          setLoading(false);
+    })
+    .catch((err) => {
+          setError(true)
+          setLoading(false);
+    });
+  }, []);
+
   return (
     <div className="app">
-      {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header> */}
       <Gallery />
+      <button type="button">Reorder</button>
     </div>
   );
 }
--- src/Gallery.js
+++ src/Gallery.js
@@ -3,30 +3,6 @@ import './Gallery.css';
 import { Photo } from './Photo.js';
 
 function Gallery(props) {
-    const url = 'http://jsonplaceholder.typicode.com/photos';
-    const [ photoData, setData ] = useState(null);
-    const [ loading, setLoading ] = useState(true);
-    const [ error, setError ] = useState(false);
-  
-    useEffect(() => {
-      setLoading(true);
-      setError(false);
-      fetch(url)
-      .then((response) => {
-            return response.json();
-      })
-      .then((photoData) => {
-            console.log(photoData[0]);
-            photoData = photoData.slice(1, 100);
-            setData(photoData);
-            setLoading(false);
-      })
-      .catch((err) => {
-            setError(true)
-            setLoading(false);
-      });
-    }, []);
-
     if (loading) {
         <div className="gallery">
             <p>LOADING...</p>
